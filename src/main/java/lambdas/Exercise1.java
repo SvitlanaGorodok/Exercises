4720b7017c3b7e4ff9867538e4186677f84a3184
svetl <svetlana.akulowa@gmail.com>
2023-04-10 14:56:42 +0300

    ex1

---
+++ src/main/java/lambdas/Exercise1.java
@@ -0,0 +1,21 @@
+package lambdas;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//1. Получить список студентов, отсортировав их по имени
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        List<Student> students = List.of(new Student(1, "Eric", "Cantona", 31),
+                new Student(2, "David", "Beckham", 28),
+                new Student(3, "Luis", "Suares", 33),
+                new Student(4, "Zinedine", "Zidane", 33));
+
+        List<Student> result = students.stream()
+                .sorted(Comparator.comparing(Student::getFirstName))
+                .collect(Collectors.toList());
+        System.out.println(result);
+    }
+}
