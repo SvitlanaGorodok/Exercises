1b04b85a3e36277a934b1faa161e1bb08220ea50
svetl <svetlana.akulowa@gmail.com>
2023-04-15 00:50:08 +0300

    Exercise 3 with trees

--- src/main/java/trees/Exercise3.java
+++ src/main/java/trees/Exercise3.java
@@ -1,33 +1,33 @@
 package trees;
 /*
-     4
-    /\
-  3  10
- /    /\
-1   6  12
-   /\
-  5  7
+     9
+    / \
+  3   14
+ /\    / \
+1  5  11  15
+     / \
+   10  12
  */
 //Задача: написати метод для виводу дерева
 public class Exercise3 {
     public static void main(String[] args) {
-        TreeNode tree = new TreeNode(4,
+        TreeNode tree = new TreeNode(9,
                         new TreeNode(3,
                                 new TreeNode(1),
-                                null),
-                        new TreeNode(10,
-                                new TreeNode(6,
-                                        new TreeNode(5),
-                                        new TreeNode(7)),
-                                new TreeNode(12)));
+                                new TreeNode(5)),
+                        new TreeNode(14,
+                                new TreeNode(11,
+                                        new TreeNode(10),
+                                        new TreeNode(12)),
+                                new TreeNode(15)));
         System.out.println(printTree(tree));
-        //3, 4, 5, 6, 7, 10, 12
+        //1, 3, 5, 9, 10, 11, 12, 14 ,15
     }
     static String printTree(TreeNode root){
         if(root == null){
             return "";
         } else {
-            return printTree(root.left)  + " " +  root.val;
+            return printTree(root.left) + root.val + " " + printTree(root.right);
         }
     }
 
